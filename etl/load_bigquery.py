aa53453439dae2fe2f02b915c53e1b0b31cda6df
aladon <124466057+aladon090@users.noreply.github.com>
2025-12-07 15:16:16 +0000

    Update BigQuery load script and optional notebook

--- etl/load_bigquery.py
+++ etl/load_bigquery.py
@@ -1,9 +1,6 @@
 from google.cloud import bigquery
 import os
-import time
-
 from datetime import datetime
-
 
 def load_parquet_to_bq(project_id, dataset_id, table_id, parquet_file):
     client = bigquery.Client(project=project_id)  # connect to BigQuery
@@ -27,9 +24,8 @@ def load_parquet_to_bq(project_id, dataset_id, table_id, parquet_file):
 
 if __name__ == "__main__":
     try:
-        startTime = datetime.now()
-        finish_time = datetime.now()
-        print(f"Loading has begun at {startTime}")
+        start_time = datetime.now()
+        print(f"Loading has begun at {start_time}")
 
         PROJECT_ID = "london-housing-480514"
         DATASET_ID = "london_blackcab"
@@ -38,8 +34,13 @@ if __name__ == "__main__":
 
         os.makedirs("Data", exist_ok=True)  # ensure folder exists
         load_parquet_to_bq(PROJECT_ID, DATASET_ID, TABLE_ID, PARQUET_FILE)
-        
-        print(f"Loading has finsh at {startTime - finish_time}")
+
+        finish_time = datetime.now()
+        elapsed_time = finish_time - start_time
+        print(f"Loading has finished at {finish_time}")
+        print(f"Elapsed time: {elapsed_time}")
+
     except Exception as e:
-        print(f"There was an error {e}")    
+        print(f"There was an error: {e}")
+
 
